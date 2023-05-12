20e9162f652bb91aefcde1d1b7243c602b51efc8
spacenoxxx <72987415+spacenoxxx@users.noreply.github.com>
2023-05-12 10:06:19 +0530

    Update 002-set-hostname.ps1

--- img/002-set-hostname.ps1
+++ img/002-set-hostname.ps1
@@ -1,12 +1,13 @@
-# Get the network adapter with InterfaceDescription containing "VirtIO"
-#Get-NetAdapter | Where-Object {$_.InterfaceDescription -like "*VirtIO*"} | Rename-NetAdapter -NewName "Ethernet"
-#$adapter = Get-NetAdapter | Where-Object {$_.InterfaceDescription -like "*VirtIO*"}
-$adapter = Get-NetAdapter | Where-Object {$_.InterfaceDescription -like "*Intel*"}
+Get-NetAdapter | Where-Object {$_.InterfaceDescription -like "*VirtIO*"} | Rename-NetAdapter -NewName "Ethernet"
+$adapter = Get-NetAdapter | Where-Object {$_.InterfaceDescription -like "*VirtIO*"}
+#$adapter = Get-NetAdapter | Where-Object {$_.InterfaceDescription -like "*Intel*"}
 
 # Set a static IP address and gateway
-$ipAddress = "192.168.101.6"
+$ipAddress = "10.1.1.202"
 $subnetMask = "255.255.255.0"
-$gateway = "192.168.101.1"
+$gateway = "10.1.1.254"
+$dnsserver = "10.1.1.201"
+$comp-name = "SRV1"
 
 # Remove any existing IP addresses and set the new one
 $adapter | Remove-NetIPAddress -Confirm:$false
@@ -16,7 +17,13 @@ New-NetIPAddress -InterfaceIndex $adapter.ifIndex -IPAddress $ipAddress -PrefixL
 $adapter | Remove-NetRoute -Confirm:$false
 New-NetRoute -InterfaceIndex $adapter.ifIndex -DestinationPrefix "0.0.0.0/0" -NextHop $gateway
 
+#Set DNS Server 
+Set-DnsClientServerAddress -InterfaceIndex $adapter.ifIndex -ServerAddresses $dnsserver
+
+$adapter | Remove-NetRoute -Confirm:$false
+New-NetRoute -InterfaceIndex $adapter.ifIndex -DestinationPrefix "0.0.0.0/0" -NextHop $gateway
+
 # Set Hostname
-Rename-Computer -NewName "PC1" -Restart
+Rename-Computer -NewName $comp-name -Restart
 
 
